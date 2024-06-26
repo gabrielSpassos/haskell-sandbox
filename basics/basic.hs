7450b2e1aea5908fb832cee5738401775c22475a
Gabriel Passos <gabrielsantos45725@gmail.com>
2024-06-26 10:27:28 -0300

    feat: add basic

--- basics/basic.hs
+++ basics/basic.hs
@@ -6,6 +6,11 @@ main = do
     putStrLn ("Not sample: " ++ show Main.negate)
     putStrLn ("Equals sample: " ++ show equals)
     putStrLn ("Not Equals sample: " ++ show notEquals)
+    putStrLn ("5 + 4.0: " ++ show add)
+    putStrLn ("Successor of 99: " ++ show (successor 99))
+    putStrLn ("Min between 3.2 and 3.1: " ++ show (minimal 3.2 3.1))
+    putStrLn ("Max between 3.2 and 3.1: " ++ show (Main.maximum 3.2 3.1))
+    putStrLn ("Custom Add 3.2 and 3.1: " ++ show (customAdd 3.2 3.1))
 
 -- create function
 factorial 0 = 1
@@ -19,3 +24,10 @@ negate = not False
 -- equality 
 equals = 7 == 8
 notEquals = 7 /= 8
+
+-- math operations
+add = 5 + 4.0
+successor n = succ n
+minimal x y = min x y
+maximum x y = max x y
+customAdd x y = (minimal x y) + (Main.maximum x y) + 1
