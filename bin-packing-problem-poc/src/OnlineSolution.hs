868c02d10ff630fbbc9c0f34190ba505762df59a
Gabriel Passos <gabrielsantos45725@gmail.com>
2024-07-29 14:57:43 -0300

    feat: add bin packing

--- bin-packing-problem-poc/src/OnlineSolution.hs
+++ bin-packing-problem-poc/src/OnlineSolution.hs
@@ -1,10 +1,14 @@
 module OnlineSolution where
 
---  When processing next item, check if it fits in the same bin as the last item. Use a new bin only if it does not.
-nextFit :: [Integer] -> Integer -> Integer
-nextFit weights binCapacity = go weights binCapacity 0 binCapacity
-  where
-    go [] _ bins _ = bins + 1
-    go (weight:rest) binRemaining bins binCapacity
-      | weight > binRemaining = go rest (binCapacity - weight) (bins + 1) binCapacity
-      | otherwise             = go rest (binRemaining - weight) bins binCapacity
+-- When processing next item, check if it fits in the same bin as the last item. Use a new bin only if it does not.
+nextFit :: [Int] -> Int -> Int
+nextFit weights binCapacity = nextFitHelper weights binCapacity binCapacity 0
+
+-- Helper function with additional parameters to keep track of the remaining capacity and bin count
+nextFitHelper :: [Int] -> Int -> Int -> Int -> Int
+-- No weights left, add + 1 because start with 0 bins
+nextFitHelper [] _ _ result = result + 1
+-- (weight:rest) get only the head of the list
+nextFitHelper (weight:rest) binCapacity binRemainingCapacity result
+  | weight > binRemainingCapacity = nextFitHelper rest binCapacity (binCapacity - weight) (result + 1)
+  | otherwise = nextFitHelper rest binCapacity (binRemainingCapacity - weight) result
--- bin-packing-problem-poc/tests/OnlineSolutionTests.hs
+++ bin-packing-problem-poc/tests/OnlineSolutionTests.hs
@@ -4,20 +4,20 @@ import OnlineSolution
 import Test.HUnit
 import qualified System.Exit as Exit
 
-testNextFixReturn3 :: Test
-testNextFixReturn3 = TestCase (assertEqual "Should return 3" 3 (nextFit [4, 8, 1, 4, 2, 1] 10))
+testNextFitReturn3 :: Test
+testNextFitReturn3 = TestCase (assertEqual "Should return 3" 3 (nextFit [4, 8, 1, 4, 2, 1] 10))
 
-testNextFixReturn4 :: Test
-testNextFixReturn4 = TestCase (assertEqual "Should return 4" 4 (nextFit [9, 8, 2, 2, 5, 4] 10))
+testNextFitReturn4 :: Test
+testNextFitReturn4 = TestCase (assertEqual "Should return 4" 4 (nextFit [9, 8, 2, 2, 5, 4] 10))
 
-testNextFixReturn5 :: Test
-testNextFixReturn5 = TestCase (assertEqual "Should return 5" 5 (nextFit [2, 5, 4, 7, 1, 3, 8] 10))
+testNextFitReturn5 :: Test
+testNextFitReturn5 = TestCase (assertEqual "Should return 5" 5 (nextFit [2, 5, 4, 7, 1, 3, 8] 10))
 
 tests :: Test
 tests = TestList [
-    TestLabel "testNextFixReturn3" testNextFixReturn3,
-    TestLabel "testNextFixReturn4" testNextFixReturn4,
-    TestLabel "testNextFixReturn5" testNextFixReturn5
+    TestLabel "testNextFitReturn3" testNextFitReturn3,
+    TestLabel "testNextFitReturn4" testNextFitReturn4,
+    TestLabel "testNextFitReturn5" testNextFitReturn5
     ]
 
 main :: IO()
